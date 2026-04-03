b013b2b98914609f1ae8ae3c5338eee6239d02ee
Diptiranjan <diptiranjansahu999@gmail.com>
2026-04-03 17:30:20 +0530

    create new crm module

---
+++ crm_leads_menu/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'CRM Leads Menu',
+    'version': '1.0',
+    'description': """
+        Adds separate Leads menu in CRM like Enterprise.
+    """,
+    'author': 'Techvizor',
+    'category': 'CRM',
+    'depends': ['crm'],
+    'data': [
+        'views/crm_leads_menu.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
