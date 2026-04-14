76a4c46fde7f79ea5c436100336f63abc8936624
Piyush Chodvadiya <piyushchodvadiya2210@gmail.com>
2026-04-14 11:10:09 +0530

    attach custom quotation report pdf when send by email

--- sales_reports/models/sale_report.py
+++ sales_reports/models/sale_report.py
@@ -1,3 +1,4 @@
+import base64
 from odoo import models, fields, api
 import re
 
@@ -32,3 +33,42 @@ class SaleOrder(models.Model):
             content = rec.note or ''
             clean = re.sub('<[^<]+?>', '', content).strip()
             rec.has_terms = bool(clean)
+
+
+
+    # Send New Quotation Report by Email
+    def action_quotation_send(self):
+        self.ensure_one()
+
+        template = self.env.ref('sale.email_template_edi_sale')
+        report = self.env.ref('sales_reports.action_unified_quotation_new')
+        pdf, _ = report._render_qweb_pdf(
+            report.report_name,
+            res_ids=[self.id]
+        )
+
+        attachment = self.env['ir.attachment'].create({
+            'name': f'Quotation-{self.name}.pdf',
+            'type': 'binary',
+            'datas': base64.b64encode(pdf),
+            'res_model': 'mail.compose.message',
+        })
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'mail.compose.message',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_model': 'sale.order',
+                'default_res_ids': [self.id],
+                'default_use_template': True,
+                'default_template_id': template.id,
+
+                # ✅ ONLY PDF ATTACHMENT, NO OTHER DEFAULTS
+                'default_attachment_ids': [(6, 0, [attachment.id])],
+
+                # 🔥 BLOCK Odoo default report completely
+                'mark_so_as_sent': True,
+            },
+        }
