db1dc9969d45a850e21768803cb0fd6beea6b328
Diptiranjan <diptiranjansahu999@gmail.com>
2026-04-14 11:44:05 +0530

    remove the module

--- invoice_report/models/terms_condition.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class TermsAndConditions(models.Model):
-    _name = "terms.and.conditions"
-    _description = "Temrs And Conditions"
-
-    name = fields.Char("Name")
-    terms_condition = fields.Html("Terms and Conditions")
