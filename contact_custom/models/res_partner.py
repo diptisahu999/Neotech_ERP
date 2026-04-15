280a80e2bb12c45644715f30b8b0871223ca64e5
Piyush Chodvadiya <piyushchodvadiya2210@gmail.com>
2026-04-15 11:52:28 +0530

    Add created by filed in Contacts

--- contact_custom/models/res_partner.py
+++ contact_custom/models/res_partner.py
@@ -4,10 +4,10 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     person_name = fields.Char(string='Contact Name')
-    # created_by_id = fields.Many2one(
-    #     'res.users',
-    #     string='Created By',
-    #     related='create_uid',
-    #     readonly=True,
-    #     store=True,
-    # )
+    created_by_id = fields.Many2one(
+        'res.users',
+        string='Created By',
+        related='create_uid',
+        readonly=True,
+        store=True,
+    )
